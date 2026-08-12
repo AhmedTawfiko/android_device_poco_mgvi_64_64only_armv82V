e92003cabc9fb46cd01edad2ed92f77a5e73986d
AhmedTawfiko <brinebloxahmed@gmail.com>
2026-08-12 06:05:07 +0300

    تعدلات

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,3 @@
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_mgvi_64_64only_armv82.mk
-COMMON_LUNCH_CHOICES := twrp_mgvi_64_64only_armv82-eng
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/fox_mgvi_64_64only_armv82.mk
+COMMON_LUNCH_CHOICES := fox_mgvi_64_64only_armv82-eng
