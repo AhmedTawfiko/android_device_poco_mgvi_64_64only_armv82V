1b8b637e4868da606516db164d17d804b81c6218
AhmedTawfiko <brinebloxahmed@gmail.com>
2026-08-12 07:33:03 +0300

    Update twrp_mgvi_64_64only_armv82.mk

--- twrp_mgvi_64_64only_armv82.mk
+++ twrp_mgvi_64_64only_armv82.mk
@@ -1,6 +1,5 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-$(call inherit-product, vendor/recovery/orangefox.mk)
 PRODUCT_NAME := twrp_mgvi_64_64only_armv82
 PRODUCT_DEVICE := mgvi_64_64only_armv82
 PRODUCT_MANUFACTURER := poco
